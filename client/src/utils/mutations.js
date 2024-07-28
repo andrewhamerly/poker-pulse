f10e6588a1343f7d0bf3df397a0bbc4ac5fcbdf2
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-07-28 10:02:07 -0400

    created a starter code for our mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,29 @@
+// This is a mutations starter. Feel free to edit and update as needed
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// We can add more mutations as needed
