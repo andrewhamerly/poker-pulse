b4d3554bc86969996f13102236be2a35804bd152
BreTre <bctrepal@gmail.com>
2024-07-28 21:42:46 -0400

    added profile section content and raw css

--- client/src/components/Profile/ProfileSection.jsx
+++ client/src/components/Profile/ProfileSection.jsx
@@ -1,7 +1,22 @@
-export default function ProfileSection() {
+export default function ProfileSection(props) {
     return (
-        <div>
-            <img src="" alt="profile picture"/>
+        <div className="profile-container">
+            <div className="pic-user-level">
+                <img src={props.userpic} alt="profile picture" />
+                <p>{props.username}</p>
+                <p>{props.pokerlevel}</p>
+            </div>
+            <p>{props.userbio}</p>
+            <div className="buttons">
+                <div className="youstake-link">
+                    <p>Youstake</p>
+                    <a href="https://youstake.com/" target="_blank">Visit</a>
+                </div>
+                <div className="hendon-link">
+                    <p>Hendon Mob</p>
+                    <a href="https://www.thehendonmob.com/" target="_blank">Visit</a>
+                </div>
+            </div>
         </div>
     )
 }
