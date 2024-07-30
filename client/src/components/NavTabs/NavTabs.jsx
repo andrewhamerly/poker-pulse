b75e6bd07a4834d5e0c87e09ed5c2cf0b263623b
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-07-29 21:26:54 -0400

    Updated Header Nav Buttons to React-Router-Dom

--- client/src/components/NavTabs/NavTabs.jsx
+++ client/src/components/NavTabs/NavTabs.jsx
@@ -5,11 +5,12 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
+    <ul className="flex flex-col md:flex-row md:space-x-4 border-b border-outerSpace bg-gunmetal p-2">
       <li className="nav-item">
         <Link
           to="/"
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          className={`px-4 py-2 ${currentPage === '/' ? 'text-white bg-hunterGreen' : 'text-whiteSmoke'}`}
+          aria-current={currentPage === '/' ? 'page' : undefined}
         >
           Home
         </Link>
@@ -17,7 +18,8 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/profile"
-          className={currentPage === '/profile' ? 'nav-link active' : 'nav-link'}
+          className={`px-4 py-2 ${currentPage === '/profile' ? 'text-white bg-hunterGreen' : 'text-whiteSmoke'}`}
+          aria-current={currentPage === '/profile' ? 'page' : undefined}
         >
           Profile
         </Link>
@@ -25,9 +27,28 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/feed"
-          className={currentPage === '/feed' ? 'nav-link active' : 'nav-link'}
+          className={`px-4 py-2 ${currentPage === '/feed' ? 'text-white bg-hunterGreen' : 'text-whiteSmoke'}`}
+          aria-current={currentPage === '/feed' ? 'page' : undefined}
         >
           Feed
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/signup"
+          className={`px-4 py-2 rounded ${currentPage === '/signup' ? 'text-white bg-hunterGreen' : 'text-eerieBlack border border-outerSpace bg-whiteSmoke transition hover:bg-rojo hover:text-gray-700 focus:outline-none focus:ring focus:ring-hunterGreen'}`}
+          aria-current={currentPage === '/signup' ? 'page' : undefined}
+        >
+          Sign up
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/login"
+          className={`px-4 py-2 rounded ${currentPage === '/login' ? 'text-white bg-hunterGreen' : 'text-eerieBlack bg-vermillion transition hover:bg-rojo focus:outline-none focus:ring focus:ring-hunterGreen'}`}
+          aria-current={currentPage === '/login' ? 'page' : undefined}
+        >
+          Sign in
         </Link>
       </li>
     </ul>
