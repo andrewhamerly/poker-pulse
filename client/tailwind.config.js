9df3a7672d8158da2bdef441c9804daae60e5fec
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-07-29 20:50:17 -0400

    Tailwind setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/**/*.jsx",
+    "./src/**/*.jsx",
+    "./src/*.jsx",
+    "*",
+  ],
   theme: {
     colors: {
       onyx: '#353B3B',
