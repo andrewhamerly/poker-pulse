0f5721c03ea5bb14a871251a7ec2565a980a097c
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-07-31 19:50:33 -0400

    Created Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-
 import Auth from '../utils/auth';
+import logo from '../assets/images/poker-pulse-logo.png';
 
-const Login = (props) => {
+const Login = () => {
   const [loginData, setLoginData] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setLoginData({
       ...loginData,
       [name]: value,
@@ -20,7 +19,6 @@ const Login = (props) => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    console.log(loginData);
     try {
       const { data } = await login({
         variables: { ...loginData },
@@ -38,47 +36,103 @@ const Login = (props) => {
   };
 
   return (
-    <div>
-      <h4>Login</h4>
-      <div className="cardBody">
-        {data ? (
-          <p>
-            Success! You may now head{' '}
-            <Link to="/"></Link>
-          </p>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <input
-              className="emailInput"
-              placeholder="Your email"
-              name="email"
-              type="email"
-              value={loginData.email}
-              onChange={handleChange}
-            />
-            <input
-              className="passwordInput"
-              placeholder="******"
-              name="password"
-              type="password"
-              value={loginData.password}
-              onChange={handleChange}
-            />
-            <button
-              className="submitButton"
-              style={{ cursor: 'pointer' }}
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
-        )}
-
-        {error && (
+    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-lg">
+        <div className="flex justify-center mb-4">
+          <Link to="/">
+            <img src={logo} alt="Poker Pulse Logo" className="logo w-36" />
+          </Link>
+        </div>
+        <h1 className="text-center text-2xl font-bold text-eerieBlack sm:text-3xl">Welcome Back Player</h1>
+        <form onSubmit={handleFormSubmit} className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
+          <p className="text-center text-lg font-medium text-outerSpace">Please enter login details below</p>
           <div>
-            {error.message}
+            <label htmlFor="email" className="sr-only">Email</label>
+            <div className="relative">
+              <input
+                type="email"
+                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-md"
+                placeholder="Enter email"
+                name="email"
+                value={loginData.email}
+                onChange={handleChange}
+              />
+              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 text-outerSpace"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+              </span>
+            </div>
           </div>
-        )}
+          <div>
+            <label htmlFor="password" className="sr-only">Password</label>
+            <div className="relative">
+              <input
+                type="password"
+                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-md"
+                placeholder="Enter password"
+                name="password"
+                value={loginData.password}
+                onChange={handleChange}
+              />
+              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  />
+                </svg>
+              </span>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="block w-full rounded-lg bg-hunterGreen px-5 py-3 text-md font-2xl font-extrabold text-whiteSmoke hover:bg-hunterGreen/75 hover:text-onyx"
+            style={{ cursor: 'pointer' }}
+          >
+            Sign in
+          </button>
+          {error && (
+            <div className="text-center text-md text-red-500 mt-2">
+              {error.message}
+            </div>
+          )}
+          {data && (
+            <p className="text-center text-md text-eerieBlack mt-2">
+              Success! You may now head{' '}
+              <Link to="/">back to the homepage.</Link>
+            </p>
+          )}
+          <p className="text-center text-md text-eerieBlack">
+            No account?<span> </span>
+            <Link className="underline font-semibold hover:text-hunterGreen" to="/signup">Sign up</Link>
+          </p>
+        </form>
       </div>
     </div>
   );
