610cb7a3a8425ffdf92022c6d371f8b8342104bb
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-07-31 12:48:04 -0400

    Updated the header mobile menu with a popout using state management.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import logo from '../../assets/images/poker-pulse-logo.png';
 import NavTabs from '../../components/NavTabs/NavTabs.jsx';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="border-b border-outerSpace bg-gunmetal">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div className="flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
-          <div>
+      <div className="mx-auto max-w-screen-xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
             <img src={logo} alt="Poker Pulse Logo" className="logo w-36" />
           </div>
-          <nav>
+          <div className="md:hidden">
+            <button 
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="text-white hover:text-hunterGreen focus:outline-none"
+            >
+                <p className="text-base font-bold">Menu</p>
+                <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}></path>
+                </svg>
+            </button>
+          </div>
+          <nav className={`md:flex ${isMenuOpen ? 'block' : 'hidden'} w-full md:w-auto`}>
             <NavTabs />
           </nav>
         </div>
--- client/src/components/NavTabs/NavTabs.jsx
+++ client/src/components/NavTabs/NavTabs.jsx
@@ -5,11 +5,11 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="flex flex-col md:flex-row md:space-x-4 border-b border-outerSpace bg-gunmetal p-2">
+    <ul className="flex flex-col md:flex-row md:space-x-4 border-t border-outerSpace md:border-t-0 bg-gunmetal p-2 md:p-0 md:border-0 md:bg-transparent w-full md:w-auto">
       <li className="nav-item">
         <Link
           to="/"
-          className={`px-4 py-2 ${currentPage === '/' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-whiteSmoke'}`}
+          className={`block px-4 py-2 ${currentPage === '/' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-whiteSmoke'}`}
           aria-current={currentPage === '/' ? 'page' : undefined}
         >
           Home
@@ -18,7 +18,7 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/profile"
-          className={`px-4 py-2 ${currentPage === '/profile' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-whiteSmoke'}`}
+          className={`block px-4 py-2 ${currentPage === '/profile' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-whiteSmoke'}`}
           aria-current={currentPage === '/profile' ? 'page' : undefined}
         >
           Profile
@@ -26,8 +26,8 @@ function NavTabs() {
       </li>
       <li className="nav-item">
         <Link
-          to="/feed"
-          className={`px-4 py-2 ${currentPage === '/schedule' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-whiteSmoke'}`}
+          to="/schedule"
+          className={`block px-4 py-2 ${currentPage === '/schedule' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-whiteSmoke'}`}
           aria-current={currentPage === '/schedule' ? 'page' : undefined}
         >
           Schedule
@@ -36,7 +36,7 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/login"
-          className={`px-4 py-2 rounded ${currentPage === '/login' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-eerieBlack bg-whiteSmoke transition hover:bg-hunterGreen hover:text-whiteSmoke focus:outline-none focus:ring focus:ring-hunterGreen'}`}
+          className={`block px-4 py-2 rounded ${currentPage === '/login' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-eerieBlack bg-whiteSmoke transition hover:bg-hunterGreen hover:text-whiteSmoke focus:outline-none focus:ring focus:ring-hunterGreen'}`}
           aria-current={currentPage === '/login' ? 'page' : undefined}
         >
           Login
@@ -45,7 +45,7 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/signup"
-          className={`px-4 py-2 rounded ${currentPage === '/signup' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-white bg-hunterGreen border border-outerSpace transition hover:bg-whiteSmoke hover:text-eerieBlack focus:outline-none focus:ring focus:ring-hunterGreen'}`}
+          className={`block px-4 py-2 rounded ${currentPage === '/signup' ? 'font-bold text-xl text-white bg-hunterGreen' : 'font-bold text-xl text-white bg-hunterGreen border border-outerSpace transition hover:bg-whiteSmoke hover:text-eerieBlack focus:outline-none focus:ring focus:ring-hunterGreen'}`}
           aria-current={currentPage === '/signup' ? 'page' : undefined}
         >
           Sign Up
