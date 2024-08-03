0a99938483fa0ba5cbd0ec02669acd549c01955f
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-08-03 14:35:10 -0400

    fixed error: Each child in a list should have a unique key prop.

--- client/src/components/Schedule/multiDay.jsx
+++ client/src/components/Schedule/multiDay.jsx
@@ -1,8 +1,9 @@
 const MultiDayValue = (multiDay) => {
   if (multiDay.multiDay === true) {
     return <div>✔</div>
-  } else
-    return <div>-</div>
+  } else if(multiDay.multiDay === false) {
+    return <div>❌</div>
+  }
 }
 
 export default MultiDayValue
