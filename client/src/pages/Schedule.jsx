e00f7235ded7c7ae3c8193b84ef745c3d182417f
BreTre <bctrepal@gmail.com>
2024-08-04 23:11:14 -0400

    fixed table styling on mobile

--- client/src/pages/Schedule.jsx
+++ client/src/pages/Schedule.jsx
@@ -1,46 +1,26 @@
 import '../components/Schedule/Schedule.css';
-import React from 'react';
 import HorizontalTable from '../components/Schedule/HorizontalTable';
 import VerticalTable from '../components/Schedule/VerticalTable';
-import { Box } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import { useQuery, useMutation } from '@apollo/client';
 import useWindowSize from '../utils/useWindowSize';
-
-import FormattedEntryFee from '../components/Schedule/formattedEntryFee';
-import GuaranteeType from '../components/Schedule/guaranteePrefix';
-import FormattedDate from '../components/Schedule/formattedDate';
-import FormattedTime from '../components/Schedule/formattedTime';
-import FormattedChips from '../components/Schedule/formattedChips';
-import FormattedLevels from '../components/Schedule/formattedLevels';
-import MultiDayValue from '../components/Schedule/multiDay';
-import HandleEventTitle from '../components/Schedule/absentTitle';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarXmark } from '@fortawesome/free-solid-svg-icons';
-
-import { GET_SCHEDULE } from '../utils/queries';
-
-import { useRemoveFromSchedule, sortByDateTime } from '../utils/scheduleHelper';
 
 
 const Schedule = () => {
-  // UNCOMMENT OUT LINES 15-30 FOR JWT AUTH WHEN APP FINISHED
-  //     const navigate = useNavigate();
+  // UNCOMMENT OUT LINES 10-24 FOR JWT AUTH WHEN APP FINISHED
+    //   const navigate = useNavigate();
 
-  //   useEffect(() => {
-  //     const token = Auth.getToken();
+    // useEffect(() => {
+    //   const token = Auth.getToken();
 
-  //     if (!token) {
-  //       navigate('/signup');
-  //     }
-  //     }, [navigate]);
+    //   if (!token) {
+    //     navigate('/signup');
+    //   }
+    //   }, [navigate]);
 
-  //     const token = Auth.getToken();
+    //   const token = Auth.getToken();
 
-  //     if (!token) {
-  //         return null;
-  //     }
+    //   if (!token) {
+    //       return null;
+    //   }
 
   const size = useWindowSize();
 
