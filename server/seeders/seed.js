7d4a9c46a2b597abf205f24e955e94e2dac1ad66
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-08-07 03:23:11 -0400

    added MONGODB_URI to seed.js as well

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -6,7 +6,7 @@ const userSeed = require('./userSeed.json');
 
 const seedDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/vegas-poker-pulse', {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/vegas-poker-pulse', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
